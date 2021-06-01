83b0db6b18653d9b39995adc4a25915ffc249c5c
Josu Igoa <josuigoa@gmail.com>
2021-06-01 15:58:20 +0200

    if all values are null, treat as empty

--- lib/model/ItemState.dart
+++ lib/model/ItemState.dart
@@ -13,22 +13,33 @@ class ItemState {
       this.id, this.text, this.textColor, this.iconData, this.bgColor);
 
   factory ItemState.fromJson(Map<String, dynamic> json) {
+    
+    var allNull = true;
+    
     Color textColor = Colors.white;
     if (json['textColor'] != null) {
       textColor = Color(int.parse(json['textColor'], radix: 16));
+      allNull = false;
     }
     Color bgColor = Colors.blueGrey.shade900;
     if (json['bgColor'] != null) {
       bgColor = Color(int.parse(json['bgColor'], radix: 16));
+      allNull = false;
     }
     Uint8List iconData;
     if (json['icon'] != null) {
       var iconBase64 = json['icon'].toString();
       iconData = base64Decode(iconBase64);
+      allNull = false;
     }
     String text = '';
     if (json['text'] != null) {
       text = json['text'].toString();
+      allNull = false;
+    }
+    
+    if (allNull) {
+      return ItemState.empty();
     }
 
     return ItemState(
