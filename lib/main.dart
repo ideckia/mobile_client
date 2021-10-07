76fc1a724b4f9f4f242403eeb2cde49dfd7a23aa
Josu Igoa <josu.igoa@indaba.lks.es>
2021-10-07 09:23:44 +0200

    fix internet access, added keep_screen_on

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:keep_screen_on/keep_screen_on.dart';
 
 import 'view/LookForIpView.dart';
 
@@ -10,17 +11,18 @@ Future<void> main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: [
-          Locale('eu'),
-          Locale('en'),
-          Locale('es'),
-          Locale('fr')
-        ],
-        path:
-            'assets/translations',
-        fallbackLocale: Locale('en'),
-        child: Ideckia()),
+      supportedLocales: [
+        Locale('eu'),
+        Locale('en'),
+        Locale('es'),
+        Locale('fr')
+      ],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: Ideckia(),
+    ),
   );
+  KeepScreenOn.turnOn();
   SystemChrome.setEnabledSystemUIOverlays([]);
 }
 
